10de0563c81c402cf7c8e53c138129fc32047e2c
ducbao414 <md231@MacBook-Air.local>
2015-08-24 20:42:42 +0700

    Created

--- SQLiteiOS/ViewController.swift
+++ SQLiteiOS/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let db = SQLiteDB.sharedInstance()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //print all records
+        printAllRecords()
+        //example using query
+        whoIsFromChina()
+        //delete some records and printAllRecords again to see the result
+        deleteRecord()
+        printAllRecords()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //print all records
+    func printAllRecords()
+    {
+        var result = db.query("select * from person", parameters: nil)
+        println("===============================")
+        for row in result
+        {
+            print(row["name"]!.asString())
+            print(" is from ")
+            println(row["country"]!.asString())
+        }
+    }
+    //example using query
+    func whoIsFromChina()
+    {
+        var result = db.query("select * from person where country = 'China'", parameters: nil)
+        println("===============================")
+        println("From China:")
+        for row in result
+        {
+            println(row["name"]!.asString())
+        }
+    }
+    //example using execute
+    func deleteRecord()
+    {
+        db.execute("delete from person where country = 'US'", parameters: nil)
+    }
 }
 
